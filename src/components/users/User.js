0fa3113385c7fadd2a8575059f919ed06e4cc992
vishal1jain <vishal1.jain@paytm.com>
2019-12-06 19:52:44 +0530

    Use effect hook

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import Spinner from '../layout/Spinner';
 import propTypes from 'prop-types';
 import Repos from '../repos/Repos';
 import { Link } from 'react-router-dom';
-class User extends Component {
-    componentDidMount() {
-        console.log(this.props)
-        this.props.getUser(this.props.match.params.login)
-        this.props.getUserRepos(this.props.match.params.login)
-    }
-    render() {
-        const { user: { name, hireable }, loading, repos } = this.props
-        return (
-            <div>
-                <Link to="/">Go to search</Link>
-                {loading ?
-                    <Spinner /> :
-                    <div>
 
-                        <h1>User: {name}</h1>
-                        Hireable:{hireable ? 'YES' : 'NO'}
-                    </div>
-                }
+const User = ({ user: { name, hireable }, loading, repos, match, getUserRepos, getUser }) => {
+    useEffect(() => {
+        getUser(match.params.login)
+        getUserRepos(match.params.login)
+        // eslint-disable-next-line
+    }, [])
+    return (
+        <div>
+            <Link to="/">Go to search</Link>
+            {loading ?
+                <Spinner /> :
+                <div>
 
-                <Repos repos={repos} />
+                    <h1>User: {name}</h1>
+                    Hireable:{hireable ? 'YES' : 'NO'}
+                </div>
+            }
 
-            </div>
-        )
-    }
+            <Repos repos={repos} />
+
+        </div>
+    )
 }
 
 User.propTypes = {
